e89bd5c8f08dd40b55ce8ac383f1cf95c85da958
hasan foraty <hasanforaty19@gmail.com>
2024-02-04 21:12:04 +0330

    add some modification

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-with open("./Input/Letters/starting_letter.txt") as start_letter:
+STARTING_LETTER = "./Input/Letters/starting_letter.txt"
+NAMES_FILE = "./Input/Names/invited_names.txt"
+
+
+with open(STARTING_LETTER) as start_letter:
     template = start_letter.readlines()
-    with open('./Input/Names/invited_names.txt') as name_file:
+    with open(NAMES_FILE) as name_file:
         names = name_file.readlines()
         for name in names:
             with open('./Output/ReadyToSend/letter_to_'+name.strip()+'.txt', 'w') as output:
